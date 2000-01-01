9dea0be557a5a2e32896f9d820637a66c0d1804f
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for ee7f84e1-a9f6-4d84-a644-a952a0ce0644-bug-5

--- internal/calibration/query.go
+++ internal/calibration/query.go
@@ -2,6 +2,7 @@ package calibration
 
 import (
 	"context"
+	"encoding/json"
 
 	"buoy-calibration-gate/internal/audit"
 	"buoy-calibration-gate/internal/repository"
@@ -22,16 +23,40 @@ func (s *Service) Timeline(ctx context.Context, id string) (audit.Timeline, erro
 	cached, found := s.timelineCache[id]
 	s.timelineMu.RUnlock()
 	if found {
-		return cached, nil
+		return cloneTimeline(cached), nil
 	}
 	timeline, err := audit.LoadTimeline(ctx, s.store, id)
 	if err != nil {
 		return timeline, err
 	}
 	if len(timeline.Events) > 0 && timeline.Events[len(timeline.Events)-1].EventType == "permit.issued" {
+		cloned := cloneTimeline(timeline)
 		s.timelineMu.Lock()
-		s.timelineCache[id] = timeline
+		s.timelineCache[id] = cloned
 		s.timelineMu.Unlock()
 	}
 	return timeline, nil
+}
+
+// cloneTimeline returns a deep copy of a timeline so that callers cannot
+// mutate the cached timeline (or its event payloads) through the returned
+// value, and vice versa. Slice headers share underlying arrays by default,
+// which previously let caller-side payload edits leak into the cache and
+// poison subsequent queries even though SQLite was untouched.
+func cloneTimeline(t audit.Timeline) audit.Timeline {
+	out := t
+	if t.Events == nil {
+		return out
+	}
+	events := make([]repository.AuditEvent, len(t.Events))
+	for i := range t.Events {
+		events[i] = t.Events[i]
+		if payload := t.Events[i].Payload; payload != nil {
+			cp := make(json.RawMessage, len(payload))
+			copy(cp, payload)
+			events[i].Payload = cp
+		}
+	}
+	out.Events = events
+	return out
 }
